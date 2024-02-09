19530e0b8ae6d6380361df0bca551ff528edb2a3
AchieverOnkar <118494882+AchieverOnkar@users.noreply.github.com>
2024-02-09 15:43:27 +0530

    Update User.java

--- src/main/java/com/sonic/demo/entity/User.java
+++ src/main/java/com/sonic/demo/entity/User.java
@@ -8,7 +8,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 
-@Entity(name = "user")
+@Entity(name = "users")
 public class User {
 
 	@Id
